e06c4f23ad12d236eaa83d7c166007ac6fa9bf93
FeriW02 <full.stack020224@gmail.com>
2024-10-09 22:21:45 +0400

    jstaskkkkk

---
+++ app.js
@@ -0,0 +1,40 @@
+// **********************************************************************
+// Task-1
+// const firstElement = document.querySelector("h1")
+// console.log(firstElement);
+// Task-2
+// document.querySelector("h1").remove()
+// const getUl = document.querySelector('ul')
+// const newLi = document.querySelector('li')
+// newLi.textContent = "TITAN RTX"
+// getUl.append(newLi)
+// Task-3
+// document.createElement("h1").textContent = 'GeForce 20 Series'
+// const getPrimary = document.querySelectorAll(".primary")
+
+// let newColor = getPrimary.style.color = "#ebebeb"
+
+// getPrimary.forEach(item => {
+//     item.style.color = "#ebebeb"
+// })
+// Task-4
+// const getPrimary = document.querySelectorAll(".primary")
+
+// console.log(getPrimary);
+
+
+// getPrimary.forEach(item => {
+//     console.log(item);
+    
+// });
+// Task-5
+// const newClass = document.querySelectorAll("li")
+
+// newClass.forEach(item=>{
+//     item.classList.add("item")
+
+//     if(!item.classList.contains("primary")){
+//         item.classList.add("secondary")
+//     }
+// })
+// **********************************************************************
